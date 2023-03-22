99301ca16d8a34f6a80178ece4b29b7981604525
faith-ogunseye <faith.akinboyewa7@gmail.com>
2023-03-22 01:22:12 +0100

    My java program

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("My name is faith");
+        System.out.println("QA Queen ");
+    }
+
+}
